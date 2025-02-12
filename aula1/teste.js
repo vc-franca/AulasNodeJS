bf6f970a061969d6ceac66dae536f5e8045e09e5
Yanpemn <victorcestafran08@gmail.com>
2025-02-12 17:20:16 -0300

    dgvpwaov

--- aula1/teste.js
+++ aula1/teste.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+/* ---------------------------------- MÉDIA --------------------------------- */
+// const nota1 = 10;
+// const nota2 = 5;
+
+// let media = (nota1 + nota2) / 2;
+// console.log(`A média é: ${media}`);
+
+/* ----------------------------------- IMC ---------------------------------- */
+// const peso = 50;
+// const altura = 1.7;
+
+// let imc = peso / (altura ** 2);
+// console.log(imc);
+
+/* ------------------------------- TEMPERATURA ------------------------------ */
+// const celsius = 25;
+
+// let fahrenheit = (celsius * 9 / 5) + 32;
+// console.log(`Temperatura em fahrenheit: ${fahrenheit}`);
+
+/* ------------------------------- PREÇO FINAL ------------------------------ */
+// const preco = 10;
+// const desconto = 0.15;
+
+// let precoFinal = preco - (preco * desconto);
+// console.log(`O preco final é: ${precoFinal}`);
